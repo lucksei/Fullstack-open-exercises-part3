f73c67137b4261e00d4dd521002522aea02df5fc
Luca Seimandi <lseimandi@colsecor.com.ar>
2025-05-19 16:28:25 -0300

    finished part 3.7 and 3.8

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const morgan = require('morgan')
+// const JSON = require('JSON')
 const app = express()
 
 app.use(express.json())
+app.use(morgan((tokens, request, response) => {
+  return [
+    tokens.method(request, response),
+    tokens.url(request, response),
+    tokens.status(request, response),
+    tokens.res(request, response, 'content-length'), '-',
+    tokens['response-time'](request, response), 'ms',
+    JSON.stringify(request.body),
+  ].join(' ')
+
+}))
 
 let persons = [
   {
