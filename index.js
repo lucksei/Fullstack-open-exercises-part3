f62b3277409cf2a4b26c372e6e35ab0562751884
Luca Seimandi <lseimandi@colsecor.com.ar>
2025-05-24 15:48:14 -0300

    finished part 3.15 and 3.16, deleting a phonebook number is reflected on the db and error is handled by its own middleware

--- index.js
+++ index.js
@@ -32,34 +32,48 @@ app.use(morgan((tokens, request, response) => {
   ].join(' ')
 }))
 
-// Initialize persons array
-let persons = [
-  {
-    "id": "1",
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": "2",
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": "3",
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": "4",
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
+// Error handler middleware
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if (error.name == 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
   }
-]
+
+  next(error)
+}
+
+// TODO: Delete this when the database integration is complete
+// // Initialize persons array
+// let persons = [
+//   {
+//     "id": "1",
+//     "name": "Arto Hellas",
+//     "number": "040-123456"
+//   },
+//   {
+//     "id": "2",
+//     "name": "Ada Lovelace",
+//     "number": "39-44-5323523"
+//   },
+//   {
+//     "id": "3",
+//     "name": "Dan Abramov",
+//     "number": "12-43-234345"
+//   },
+//   {
+//     "id": "4",
+//     "name": "Mary Poppendieck",
+//     "number": "39-23-6423122"
+//   }
+// ]
 
 // ##############
 // *** Routes ***
 // ##############
 
+// NOTE: Does not use mongoDB
 app.get('/info', (request, response) => {
   const timestamp = new Date(Date.now());
   response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${timestamp.toTimeString()}</p>`)
@@ -71,17 +85,24 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  Person.findById(id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -110,6 +131,8 @@ app.post('/api/persons', (request, response) => {
     })
 
 })
+
+app.use(errorHandler)
 
 const PORT = 3001
 app.listen(PORT, () => {
