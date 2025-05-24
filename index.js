e72562ecd28f76e56d96ac2b458f601561c9be82
Luca Seimandi <lseimandi@colsecor.com.ar>
2025-05-24 18:15:09 -0300

    finished part 3.19 and minor debugging session

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ const app = express()
 // *** Middlewares ***
 // ###################
 
+// Enable CORS
+app.use(cors())
+
 // Parse incoming requests with JSON payloads
 app.use(express.json())
 
-// Enable CORS
-app.use(cors())
 
 // serve frontend static files
 app.use(express.static('dist'))
@@ -37,37 +38,14 @@ app.use(morgan((tokens, request, response) => {
 const errorHandler = (error, request, response, next) => {
   console.log(error.message)
 
-  if (error.name == 'CastError') {
+  if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
   }
 
   next(error)
 }
-
-// TODO: Delete this when the database integration is complete
-// // Initialize persons array
-// let persons = [
-//   {
-//     "id": "1",
-//     "name": "Arto Hellas",
-//     "number": "040-123456"
-//   },
-//   {
-//     "id": "2",
-//     "name": "Ada Lovelace",
-//     "number": "39-44-5323523"
-//   },
-//   {
-//     "id": "3",
-//     "name": "Dan Abramov",
-//     "number": "12-43-234345"
-//   },
-//   {
-//     "id": "4",
-//     "name": "Mary Poppendieck",
-//     "number": "39-23-6423122"
-//   }
-// ]
 
 // ##############
 // *** Routes ***
@@ -79,10 +57,12 @@ app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${timestamp.toTimeString()}</p>`)
 })
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons)
-  })
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then(persons => {
+      response.json(persons)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -97,15 +77,15 @@ app.get('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  Person.findByIdAndDelete(request.params.id)
+app.delete('/api/persons/:id', (request, response, next) => {
+  return Person.findByIdAndDelete(request.params.id)
     .then(result => {
       response.status(204).end()
     })
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   let newPerson = new Person({
     name: request.body.name,
     number: request.body.number
@@ -118,10 +98,7 @@ app.post('/api/persons', (request, response) => {
     return response.status(400).json({ "error": "number is missing" })
   }
 
-  // if (persons.map(p => p.name).includes(newPerson.name)) {
-  //   return response.status(400).json({ "error": "name must be unique" })
-  // }
-  // if (persons.map(p => p.number).includes(newPerson.number)) {
+  // if (Person.find({ number: newPerson.number })) {
   //   return response.status(400).json({ "error": "number must be unique" })
   // }
 
@@ -129,10 +106,10 @@ app.post('/api/persons', (request, response) => {
     .then(savedPerson => {
       response.json(savedPerson)
     })
-
+    .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
       if (!person) {
